b645affb9ed3c54b79a65ff537a000fbe8e49074
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2024-06-10 21:44:29 +0700

    load and run dummy recommender

--- api/main.py
+++ api/main.py
@@ -1,11 +1,27 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, HTTPException, Depends
 
+from recommender.base import Recommender
+from recommender.dummy import DummyRecommender
 
-app = FastAPI()
+
+LIMIT = 20
+recommend_engine: Recommender = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global recommend_engine
+    recommend_engine = DummyRecommender()
+    try:
+        yield
+    finally:
+        recommend_engine = None
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/recommendations")
 def get_recommendations(user_id: int, returnMetadata: bool=False):
-    return {'items': [
-        {'id': 74510},
-        {'id': 76157},
-    ]}
+    return {'items': [{'id': movie_id} for movie_id in recommend_engine.get_recommended_movies(user_id, LIMIT)]}
